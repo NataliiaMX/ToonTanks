676fef5eacf5a355745134f4a4f4e384df49e9bc
Nataliia <natalia.egorova978@gmail.com>
2023-10-27 09:07:39 +0400

    First commit

---
+++ Source/ToonTanks/BasePawn.h
@@ -0,0 +1,28 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BasePawn.generated.h"
+
+UCLASS()
+class TOONTANKS_API ABasePawn : public APawn
+{
+	GENERATED_BODY() //must remain on line 12
+
+public:
+	ABasePawn(); //class constructor
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+    UPROPERTY()
+	class UCapsuleComponent* CapsuleComp; //using forward declaration
+};
